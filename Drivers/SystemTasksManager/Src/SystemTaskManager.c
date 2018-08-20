fdde62667d24ee9a0395a8026ea216fbf5963ebe
Unknown <monster_hunter_x_games@yahoo.co.jp>
2018-08-20 09:38:37 +0900

    実機テストでの問題点を修正
    修正コミットで修正した点を実機でテストし、問題が起きた点を修正致しました。

--- Drivers/SystemTasksManager/Src/SystemTaskManager.c
+++ Drivers/SystemTasksManager/Src/SystemTaskManager.c
@@ -281,12 +281,12 @@ void HAL_UART_TxCpltCallback(UART_HandleTypeDef *UartHandle){
   MW_messageTransitionCompletedCallBack();
 }
 
-void HAL_I2C_MasterRxCpltCallback(I2C_HandleTypeDef *hi2c){
+void HAL_I2C_MasterTxCpltCallback(I2C_HandleTypeDef *hi2c){
   UNUSED(hi2c);
   MW_I2C2TransitionCompletedCallBack();
 }
 
-void HAL_I2C_MasterTxCpltCallback(I2C_HandleTypeDef *hi2c){
+void HAL_I2C_MasterRxCpltCallback(I2C_HandleTypeDef *hi2c){
   UNUSED(hi2c);
   MW_I2C2ReceptionCompletedCallBack();
 }
